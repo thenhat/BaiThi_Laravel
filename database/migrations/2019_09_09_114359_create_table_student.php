d565a7701ffb24c0d151adce3c849f5649839be4
The Nhat <nhatntth1809023@fpt.edu.vn>
2019-09-09 19:57:07 +0700

    bai thi

---
+++ database/migrations/2019_09_09_114359_create_table_student.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableStudent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->tinyInteger('age');
+            $table->string('address');
+            $table->string('telephone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
